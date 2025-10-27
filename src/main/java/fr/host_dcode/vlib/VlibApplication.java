3c127fb81ab5af4df7e5920cc8d0e84408688514
DCodeProg <danael.legrand@outlook.fr>
2025-10-27 21:45:27 +0100

    first commit

---
+++ src/main/java/fr/host_dcode/vlib/VlibApplication.java
@@ -0,0 +1,13 @@
+package fr.host_dcode.vlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/host_dcode/vlib/VlibApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.host_dcode.vlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
