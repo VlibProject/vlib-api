824a68a4da0c1b989c396afebe80b816dc6d5323
Sébastien Lotten <sebastienlotten@proton.me>
2025-11-12 20:15:40 +0100

    chore: modify the id type to use uuid

--- src/main/java/fr/host_dcode/vlib/model/User.java
+++ src/main/java/fr/host_dcode/vlib/model/User.java
@@ -14,8 +14,8 @@ public class User {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
     private String email;
     private String password;
     private Status status;
