aca310fc39aef94a04f0da3cd4aa6d3f59b5aa6a
Sébastien Lotten <sebastienlotten@proton.me>
2025-11-30 20:16:31 +0100

    feat: add station filter to verify if stations are already in db

--- src/main/java/fr/host_dcode/vlib/model/Station.java
+++ src/main/java/fr/host_dcode/vlib/model/Station.java
@@ -23,9 +23,6 @@ public class Station {
     @Column(name = "last_update")
     private LocalDateTime lastUpdate;
 
-    public Station() {
-    }
-
     public Station(String name, String recordId, String stationCode, String city, double latitude, double longitude,
             String description) {
         this.name = name;
