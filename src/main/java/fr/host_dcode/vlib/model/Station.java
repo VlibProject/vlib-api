c7ef70704d14ff5b9759a651d9f3bfc636e23b97
DCodeProg <danael.legrand@outlook.fr>
2025-12-02 09:43:31 +0100

    fix: remove unused import for CreationTimestamp in Station class

--- src/main/java/fr/host_dcode/vlib/model/Station.java
+++ src/main/java/fr/host_dcode/vlib/model/Station.java
@@ -1,7 +1,6 @@
 package fr.host_dcode.vlib.model;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
