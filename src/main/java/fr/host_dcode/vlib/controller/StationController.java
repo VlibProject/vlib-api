21a0b5786ba976a2ec6181a77df02f2eb239446c
Sébastien Lotten <129305476+SHACRAM@users.noreply.github.com>
2025-12-10 19:04:41 +0100

    Feat/9 add paginating on api response (#23)
    * feat: add pagination on getAll endpoint
    
    * feat: add Page type and pageable parm on all get endpoint

--- src/main/java/fr/host_dcode/vlib/controller/StationController.java
+++ src/main/java/fr/host_dcode/vlib/controller/StationController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Sort;
 import java.util.List;
 
 @RequestMapping("/api/v1/station")
 @RestController
 public class StationController {
-
+ 
     private final StationService stationService;
     public StationController(StationService stationService) {
         this.stationService = stationService;
@@ -20,17 +25,19 @@ public class StationController {
 
 
     @GetMapping("/")
-    public List<Station> getAllStations(){
-        return stationService.getAll();
+    public Page<Station> getAllStations(@PageableDefault(size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable){
+        return stationService.getAll(pageable);
     }
 
     @GetMapping("/search/")
-    public List<Station> searchByCriteria(
+    public Page<Station> searchByCriteria(
         @RequestParam(required = false) String name, 
         @RequestParam(required = false) String city,
-        @RequestParam(required = false) String station_code){
-            return stationService.searchByCriteria(name, city, station_code); 
+        @RequestParam(required = false) String station_code,
+        @PageableDefault(size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable){
+            return stationService.searchByCriteria(name, city, station_code, pageable); 
     }
+
     @PutMapping("/update/{id}")
     public Station updateStation(@RequestBody Station station, @PathVariable("id") String id){
         return stationService.updateStation(station, id);
